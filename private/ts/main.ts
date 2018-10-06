eaa5f4f4a092222a73be77d6e29589d7f911c6c5
Flight <flight.neg@gmail.com>
2018-10-06 16:47:28 +0300

    Order modal added

--- private/ts/main.ts
+++ private/ts/main.ts
@@ -1,3 +1,58 @@
-$((): void => {
+interface OrderFormField {
+    id: string;
+    name: string;
+}
 
+interface OrderFormModel {
+    id: string;
+    name: string;
+    metals: Array<OrderFormField>;
+    sides: Array<OrderFormField>;
+    price: string;
+}
+
+interface OrderFormData {
+    models: Array<OrderFormModel>;
+}
+
+const orderFormInit = (): void => {
+    const orderFormModels: Array<OrderFormModel> = JSON.parse($('#order-form-json').html()).models;
+    const orderFormTemplateElement = $('#order-form-template');
+    const orderFormTemplate: any = $($.parseHTML(orderFormTemplateElement.html()));
+    const orderFormModel = $('#order-form-model');
+    const orderFormModelArray = [];
+
+    orderFormModels.forEach((model: OrderFormModel) => {
+        orderFormModelArray.push(`<option value="${model.id}">${model.name}</option>`);
+    });
+
+    orderFormModel.html(orderFormModelArray.join(''));
+
+    orderFormModel.on('change', (event: JQuery.Event) => {
+        const modelId = $(event.currentTarget).val();
+        const currentModel = orderFormModels.find((model: OrderFormModel) => model.id === modelId);
+        const orderFormMetalArray = [];
+        const orderFormSideArray = [];
+
+        currentModel.metals.forEach((model: OrderFormModel) => {
+            orderFormMetalArray.push(`<option value="${model.id}">${model.name}</option>`);
+        });
+        orderFormTemplate.find('#order-form-metal').html(orderFormMetalArray.join(''));
+
+        currentModel.sides.forEach((model: OrderFormModel) => {
+            orderFormSideArray.push(`<option value="${model.id}">${model.name}</option>`);
+        });
+        orderFormTemplate.find('#order-form-side').html(orderFormSideArray.join(''));
+
+        orderFormTemplate.find('#order-form-price').html(currentModel.price);
+
+        $('#order-form-template').replaceWith(orderFormTemplate);
+
+        $('.order-gallery').attr('hidden', 'hidden');
+        $('#order-gallery-' + modelId).removeAttr('hidden');
+    }).change();
+};
+
+$((): void => {
+    orderFormInit();
 });
